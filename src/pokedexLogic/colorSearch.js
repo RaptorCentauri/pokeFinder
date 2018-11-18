2b70e75888f85b567880786756a663c0a00ef6e7
RaptorCentauri <dave.leo.shilander@icloud.com>
2018-11-17 22:14:15 -0600

    colorSearch now returns a sorted array of IDs

--- src/pokedexLogic/colorSearch.js
+++ src/pokedexLogic/colorSearch.js
@@ -1,30 +1,3 @@
-// //Search by specific color
-// function colorSearch(color){
-// 	console.log('running color search');
-// 	if (!color){
-// 		console.log('no color');
-// 		ajaxColor.resolve();
-// 	}
-//
-// 	else{
-// 		$.ajax({
-// 			url: `https://pokeapi.co/api/v2/pokemon-color/${color}/`,
-// 			method: "GET"
-// 		}).done(function(data) {
-// 			// console.log(data);
-// 			for (var i = 0; i < data.pokemon_species.length; i++) {
-// 				colorArray.push(data.pokemon_species[i].name);
-// 			}
-// 			console.log(colorArray);
-// 			ajaxColor.resolve();
-// 		}).fail(function(){
-// 			$("#results").html("The API is not responding");
-// 			$("#status-light").css("background-color", "#ff1c1c");
-// 		});;
-// 	}
-// }
-
-
 import axios from 'axios';
 
 const searchColor = async (searchColor) => {
@@ -33,24 +6,27 @@ const searchColor = async (searchColor) => {
 	try {
 		let response = await axios.get(`https://pokeapi.co/api/v2/pokemon-color/${searchColor}/`);
 
-		// console.log(response);
-
 		let pokemons = response.data.pokemon_species;
 
-		// console.log(pokemons);
-
-
 		for (let pokemon in pokemons) {
-			colorResults.push(pokemons[pokemon].name)
+			let pokemonID = +pokemons[pokemon].url.split('/')[6]
+			colorResults.push(pokemonID)
 		}
 
-		console.log(colorResults);
-		// return colorResults;
+
+		colorResults.sort((a,b) => a-b);
+		// console.table(colorResults);
+
+
+
+		return colorResults;
 
 	} catch (err) {
 		console.log(err);
 		// return err
 	}
+
+
 
 
 
