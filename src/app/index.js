eb72dc91b4f735be9ff783ef2709bd10ce3c9601
RaptorCentauri <dave.leo.shilander@icloud.com>
2018-11-19 15:14:35 -0600

    automaticallty updating search based on params

--- src/app/index.js
+++ src/app/index.js
@@ -3,51 +3,64 @@ import "babel-polyfill";
 import {render} from 'react-dom';
 import React from 'react';
 
-import colorSearch from '../pokedexLogic/colorSearch';
-import searchType from '../pokedexLogic/typeSearch';
-import searchRegion from '../pokedexLogic/regionSearch';
-import search from '../pokedexLogic/search.js'
-
-
 import getColorChoices from '../pokedexLogic/getColorChoices'
 import getRegionChoices from '../pokedexLogic/getRegionChoices'
 import getTypeChoices from '../pokedexLogic/getTypeChoices'
+import search from '../pokedexLogic/search.js'
+import searchID from '../pokedexLogic/searchID.js'
+
+import ChoiceButtons from './components/choiceButtons.js'
+
 // import searchType from '../pokedexLogic/typeSearch';
 
 class App extends React.Component{
     state = {
-      colorsChoices: [],
+      colorChoices: [],
       typeChoices: [],
       regionChoices: [],
-      searchResults: []
+      searchResults: [],
+      chosenColor: null,
+      chosenType: null,
+      chosenRegion: null
     }
 
-    componentDidMount = () => {
-      getColorChoices()
-      .then(colors => this.setState({colorsChoices: colors}));
 
-      getTypeChoices()
-      .then(types => this.setState({typeChoices: types}));
-
-      getRegionChoices()
-      .then(regions => this.setState({regionChoices: regions}));
-
-
-      // colorSearch('red')
-      // // .then()
-      //
-      // searchType('fire')
-      //
-      // searchRegion('kanto')
-      // .then(result => this.setState({searchResults: result}))
-
-      search('blue', 'water', 'kanto')
-      // .then(result => this.setState({searchResults: result}))
-      // .then(result => console.log(result))
-
+  componentDidUpdate = async (prevProps, prevState) => {
+    if(prevState.chosenColor != this.state.chosenColor || prevState.chosenType != this.state.chosenType || prevState.chosenRegion != this.state.chosenRegion){
+      if(this.state.chosenColor || this.state.chosenType || this.state.chosenRegion){
+        let ids = await search(this.state.chosenColor, this.state.chosenType, this.state.chosenRegion)
+        // console.log(ids);
+        let results = await searchID(ids)
+        this.setState({searchResults: results})
+      }
+      else{
+        this.setState({searchResults: []})
+      }
 
     }
+  }
 
+    componentDidMount = async () => {
+      let colorChoices = await getColorChoices()
+      this.setState({colorChoices: colorChoices})
+
+      let typeChoices = await getTypeChoices()
+      this.setState({typeChoices: typeChoices});
+
+      let regionChoices = await getRegionChoices()
+      this.setState({regionChoices: regionChoices});
+    }
+
+    handleChooseColor = (color) => {color != this.state.chosenColor ? this.setState({chosenColor: color}) : this.setState({chosenColor: null}) }
+    handleChooseType = (type) => {type != this.state.chosenType ? this.setState({chosenType: type}) : this.setState({chosenType: null}) }
+    handleChooseRegion = (region) => {region != this.state.chosenRegion ? this.setState({chosenRegion: region}) : this.setState({chosenRegion: null}) }
+
+
+
+
+
+    // handleChooseType = (type) => this.setState({chosenType: type})
+    // handleChooseRegion = (region) => this.setState({chosenRegion: region})
 
 
 
@@ -55,14 +68,40 @@ class App extends React.Component{
         return(
             <div className='App'>
               PokeFinder!
-              <br></br>
-              {this.state.colorsChoices}
-              <br></br>
-              {this.state.typeChoices}
-              <br></br>
-              {this.state.regionChoices}
-              <br></br>
-              {this.state.searchResults.length}
+              <div>
+                COLOR:
+              {this.state.chosenColor}
+            </div>
+            <div>
+              TYPE:
+              {this.state.chosenType}
+            </div>
+            <div>
+              REGION:
+              {this.state.chosenRegion}
+              </div>
+
+              <div>
+              {this.state.colorChoices.map((color, index) =>   <ChoiceButtons handleClick={()=>{this.handleChooseColor(color)}} name={color} key={index}/>)}
+            </div>
+            <div>
+              {this.state.typeChoices.map((type, index) =>   <ChoiceButtons handleClick={()=>{this.handleChooseType(type)}} name={type} key={index}/>)}
+            </div>
+            <div>
+              {this.state.regionChoices.map((region, index) =>   <ChoiceButtons handleClick={()=>{this.handleChooseRegion(region)}} name={region} key={index}/>)}
+            </div>
+
+
+              <div>
+                RESULT:
+                {this.state.searchResults.map((result, index) =>   <ChoiceButtons name={result.name} key={index}/>)}
+              </div>
+
+
+
+
+
+
             </div>
         );
     }
